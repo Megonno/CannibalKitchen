af831929b712ea9ea74e48218effe27fb0f60dc1
Megonno <megonno@proton.me>
2024-07-02 15:18:07 +0200

    fix item name

--- src/main/kotlin/de/megonno/cannibalkitchen/register/Items.kt
+++ src/main/kotlin/de/megonno/cannibalkitchen/register/Items.kt
@@ -1,6 +1,7 @@
 package de.megonno.cannibalkitchen.register
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.TextDecoration
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 
@@ -8,7 +9,7 @@ object Items {
     val burgerPan = {
         ItemStack(Material.BREAD).apply {
             itemMeta = itemMeta.apply {
-                displayName(Component.text("Burger Pan"))
+                displayName(Component.text("Burger Pan").decoration(TextDecoration.ITALIC, false))
                 itemName(Component.text("burger_pan"))
             }
         }
@@ -17,7 +18,7 @@ object Items {
     val burgerPetty = {
         ItemStack(Material.COOKED_BEEF).apply {
             itemMeta = itemMeta.apply {
-                displayName(Component.text("Burger Petty"))
+                displayName(Component.text("Burger Petty").decoration(TextDecoration.ITALIC, false))
                 itemName(Component.text("burger_petty"))
             }
         }
