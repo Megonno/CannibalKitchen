c519d66b103acb20aab6eb0904410cd8ec7c68b5
Megonno <megonno@proton.me>
2024-07-06 20:16:04 +0200

    changed start to game command and added stop command

--- src/main/kotlin/de/megonno/cannibalkitchen/commands/GameCommand.kt
+++ src/main/kotlin/de/megonno/cannibalkitchen/commands/GameCommand.kt
@@ -5,24 +5,42 @@ import de.megonno.cannibalkitchen.CannibalKitchen
 import de.megonno.cannibalkitchen.game.state.GameState
 import io.papermc.paper.command.brigadier.Commands
 import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.format.TextColor
+import net.kyori.adventure.text.format.NamedTextColor
 
 @Suppress("UnstableApiUsage")
 fun Commands.registerStartCommand(plugin: CannibalKitchen) {
     register(
-        Commands.literal("start")
-            .executes { context ->
-                if (plugin.gameManager.gameStateHandler.tryToChangeGameState(GameState.Starting)) {
-                    context.source.sender.sendMessage(
-                        Component.text("Game is started").color(TextColor.fromHexString("#00ff00"))
-                    )
-                } else {
-                    context.source.sender.sendMessage(
-                        Component.text("Game could not be started").color(TextColor.fromHexString("#ff0000"))
-                    )
-                }
-                Command.SINGLE_SUCCESS
-            }
+        Commands.literal("game")
+            .then(
+                Commands.literal("start")
+                    .executes { context ->
+                        if (plugin.gameManager.gameStateHandler.tryToChangeGameState(GameState.Starting)) {
+                            context.source.sender.sendMessage(
+                                Component.text("Game is started").color(NamedTextColor.GREEN)
+                            )
+                        } else {
+                            context.source.sender.sendMessage(
+                                Component.text("Game could not be started").color(NamedTextColor.RED)
+                            )
+                        }
+                        Command.SINGLE_SUCCESS
+                    }
+            )
+            .then(
+                Commands.literal("stop")
+                    .executes { context ->
+                        if (plugin.gameManager.gameStateHandler.tryToChangeGameState(GameState.Stopping)) {
+                            context.source.sender.sendMessage(
+                                Component.text("Game is stopped").color(NamedTextColor.GREEN)
+                            )
+                        } else {
+                            context.source.sender.sendMessage(
+                                Component.text("Game could not be stopped").color(NamedTextColor.RED)
+                            )
+                        }
+                        Command.SINGLE_SUCCESS
+                    }
+            )
             .build(),
         "Start the game",
         listOf()
