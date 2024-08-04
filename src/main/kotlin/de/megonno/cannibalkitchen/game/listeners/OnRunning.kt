84b02c544104ffad7f20df74f01c56d57a1849e6
Megonno <megonno@proton.me>
2024-08-04 13:21:50 +0200

    added scoreboard/bossbar join/leave behavior

--- src/main/kotlin/de/megonno/cannibalkitchen/game/listeners/OnRunning.kt
+++ src/main/kotlin/de/megonno/cannibalkitchen/game/listeners/OnRunning.kt
@@ -1,12 +1,21 @@
 package de.megonno.cannibalkitchen.game.listeners
 
+import com.destroystokyo.paper.event.block.BlockDestroyEvent
 import de.megonno.cannibalkitchen.CannibalKitchen
 import de.megonno.cannibalkitchen.game.state.GameState
 import de.megonno.cannibalkitchen.game.state.GameStateChangeEvent
+import de.megonno.cannibalkitchen.register.Displays
+import org.bukkit.Material
+import org.bukkit.block.data.Ageable
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.block.BlockGrowEvent
 import org.bukkit.event.player.PlayerItemHeldEvent
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
+import org.bukkit.inventory.ItemStack
 
 class OnRunning(private val plugin: CannibalKitchen) : Listener {
     private val gameManager = plugin.gameManager
@@ -27,6 +36,23 @@ class OnRunning(private val plugin: CannibalKitchen) : Listener {
             event.isCancelled = true
         }
     }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+        if (gameManager.gameStateHandler.gameState != GameState.Running) return
+
+        Displays.orderBossBar(event.player)
+        Displays.gameScoreboard(event.player)
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    fun onPlayerLeave(event: PlayerQuitEvent) {
+        if (gameManager.gameStateHandler.gameState != GameState.Running) return
+
+        plugin.bossBarWorkers[event.player.uniqueId]?.destroy()
+        plugin.scoreboardWorkers[event.player.uniqueId]?.destroy()
+    }
+
     @EventHandler(priority = EventPriority.HIGH)
     fun onPlayerBreakBlock(event: BlockBreakEvent) {
         if (!gameManager.gameStateHandler.isGameState(GameState.Starting, GameState.Running, GameState.Stopping)) {
